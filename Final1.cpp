f4ecb0ef8f4189cd95982fd7556ba8e32c3df3d7
Nicole C. <nmcanson@gmail.com>
2025-12-10 00:35:53 -0800

    Milestone 2

--- Final1.cpp
+++ Final1.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 int main() {
+
+    // Milestone 1
     map<string,int> traffic;
     ifstream file("210-final-1-FA25.txt");
     string a, b;
@@ -14,11 +16,15 @@ int main() {
     }
     for (auto &p : traffic) cout << p.first << " " << p.second << endl;
 
+    // Milestone 2
     int maxCount = 0;
     for (auto &p : traffic) {
         if (p.second > maxCount)
         maxCount = p.second;
     }
-    cout << "\nBusiest airport(s): " << maxCount << endl;
-
+    cout << "\nBusiest airport(s) with count " << maxCount << ":\n";
+    for (auto &p : traffic) {
+        if (p.second == maxCount)
+            cout << p.first << " " << p.second << endl;
+    }
 }
